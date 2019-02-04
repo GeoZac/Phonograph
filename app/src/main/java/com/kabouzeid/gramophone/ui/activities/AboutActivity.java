26fbb087276770fec8147536dd1fee5824bd47b5
George Zacharia <george.zcharia@gmail.com>
2019-02-04 14:07:40 +0530

    Remove donation references

--- app/src/main/java/com/kabouzeid/gramophone/ui/activities/AboutActivity.java
+++ app/src/main/java/com/kabouzeid/gramophone/ui/activities/AboutActivity.java
@@ -18,7 +18,6 @@ import com.kabouzeid.appthemehelper.ThemeStore;
 import com.kabouzeid.gramophone.App;
 import com.kabouzeid.gramophone.R;
 import com.kabouzeid.gramophone.dialogs.ChangelogDialog;
-import com.kabouzeid.gramophone.dialogs.DonationsDialog;
 import com.kabouzeid.gramophone.ui.activities.base.AbsBaseActivity;
 import com.kabouzeid.gramophone.ui.activities.intro.AppIntroActivity;
 
@@ -76,8 +75,6 @@ public class AboutActivity extends AbsBaseActivity implements View.OnClickListen
     LinearLayout visitWebsite;
     @BindView(R.id.translate)
     LinearLayout translate;
-    @BindView(R.id.donate)
-    LinearLayout donate;
     @BindView(R.id.rate_on_google_play)
     LinearLayout rateOnGooglePlay;
     @BindView(R.id.aidan_follestad_google_plus)
@@ -142,7 +139,6 @@ public class AboutActivity extends AbsBaseActivity implements View.OnClickListen
         writeAnEmail.setOnClickListener(this);
         translate.setOnClickListener(this);
         rateOnGooglePlay.setOnClickListener(this);
-        donate.setOnClickListener(this);
         aidanFollestadGooglePlus.setOnClickListener(this);
         aidanFollestadGitHub.setOnClickListener(this);
         michaelCookGooglePlus.setOnClickListener(this);
@@ -197,10 +193,6 @@ public class AboutActivity extends AbsBaseActivity implements View.OnClickListen
             openUrl(TRANSLATE);
         } else if (v == rateOnGooglePlay) {
             openUrl(RATE_ON_GOOGLE_PLAY);
-        } else if (v == donate) {
-            if (App.isProVersion()) {
-                DonationsDialog.create().show(getSupportFragmentManager(), "DONATION_DIALOG");
-            }
         } else if (v == aidanFollestadGooglePlus) {
             openUrl(AIDAN_FOLLESTAD_GOOGLE_PLUS);
         } else if (v == aidanFollestadGitHub) {
